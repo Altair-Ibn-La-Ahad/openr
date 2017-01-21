5ce91b973c600f9902516b9e0846679fdcf01623
Marcin Las <las.marcin.94@gmail.com>
2017-01-21 12:59:08 +0100

    Code refactoring, removed duplicates

--- gui/src/main/java/pl/greywarden/openr/gui/dialogs/CreateFileDialog.java
+++ gui/src/main/java/pl/greywarden/openr/gui/dialogs/CreateFileDialog.java
@@ -10,36 +10,84 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
+import javafx.util.Callback;
 import javafx.util.Pair;
+import lombok.Getter;
 import pl.greywarden.openr.gui.directoryview.DirectoryView;
 import pl.greywarden.openr.i18n.I18nManager;
 import pl.greywarden.openr.templates.Template;
 
 import java.io.File;
 import java.util.Optional;
+import java.util.function.Consumer;
 
 
 public class CreateFileDialog extends Dialog<Pair<String, String>> {
 
+    @Getter
+    private GridPane grid;
+    protected I18nManager i18n = I18nManager.getInstance();
+    protected ComboBox<String> pathComboBox;
+    private TextField filename;
+    private ButtonType create;
+
+    protected final DirectoryView left;
+    protected final DirectoryView right;
+    private final Template template;
+
     @SuppressWarnings("unchecked")
     public CreateFileDialog(Template template, DirectoryView left, DirectoryView right) {
-        I18nManager i18n = I18nManager.getInstance();
-        i18n.setBundle("create-file-dialog");
-        super.setTitle(i18n.getString(template.getName()));
-        super.setHeaderText(i18n.getString("header"));
+        this.left = left;
+        this.right = right;
+        this.template = template;
 
-        ButtonType create = new ButtonType(i18n.getString("create"), ButtonBar.ButtonData.OK_DONE);
-        ButtonType cancel = new ButtonType(i18n.getString("cancel"), ButtonBar.ButtonData.CANCEL_CLOSE);
-        super.getDialogPane().getButtonTypes().addAll(create, cancel);
+        createHeaderAndTitle();
+        createGridPane();
+        createDialogContent();
+        createOkCancelOptions();
+        setLayout();
 
-        GridPane grid = new GridPane();
+        super.getDialogPane().setContent(grid);
+
+        showDialog();
+    }
+
+    private void showDialog() {
+        Optional<Pair<String, String>> result = showAndWait();
+        result.ifPresent(confirmHandler());
+    }
+
+    private void createGridPane() {
+        grid = new GridPane();
         grid.setHgap(10);
         grid.setVgap(10);
         grid.setPadding(new Insets(20, 10, 10, 10));
+    }
 
-        TextField filename = new TextField();
+    private void createOkCancelOptions() {
+        create = new ButtonType(i18n.getString("create"), ButtonBar.ButtonData.OK_DONE);
+        ButtonType cancel = new ButtonType(i18n.getString("cancel"), ButtonBar.ButtonData.CANCEL_CLOSE);
+
+        super.getDialogPane().getButtonTypes().addAll(create, cancel);
+        super.setResultConverter(createResultConverter());
+
+        Node createButton = super.getDialogPane().lookupButton(create);
+        filename.textProperty().addListener((observable, oldValue, newValue) ->
+                createButton.setDisable(newValue.trim().isEmpty()));
+        createButton.setDisable(true);
+        filename.requestFocus();
+    }
+
+    private void setLayout() {
+        ColumnConstraints stretch = new ColumnConstraints();
+        stretch.setFillWidth(true);
+        grid.getColumnConstraints().addAll(stretch, stretch);
+    }
+
+    protected void createDialogContent() {
+        filename = new TextField();
         filename.setPromptText(i18n.getString("filename"));
-        ComboBox<String> pathComboBox = new ComboBox<>();
+        pathComboBox = new ComboBox<>();
         pathComboBox.getItems().addAll(left.getRootPath(), right.getRootPath());
         pathComboBox.getSelectionModel().select(0);
 
@@ -48,12 +96,19 @@ public class CreateFileDialog extends Dialog<Pair<String, String>> {
         grid.add(new Label(i18n.getString("path") + ":"), 0, 1);
         grid.add(pathComboBox, 1, 1);
 
-        ColumnConstraints stretch = new ColumnConstraints();
-        stretch.setFillWidth(true);
-        grid.getColumnConstraints().addAll(stretch, stretch);
+        filename.setMinWidth(400);
+        pathComboBox.setMinWidth(400);
+    }
 
-        super.setResultConverter(dialogButton -> {
-            if (dialogButton == create) {
+    protected void createHeaderAndTitle() {
+        i18n.setBundle("create-file-dialog");
+        super.setTitle(i18n.getString(template.getName()));
+        super.setHeaderText(i18n.getString("header"));
+    }
+
+    private Callback<ButtonType, Pair<String, String>> createResultConverter() {
+        return param -> {
+            if (param == create) {
                 File target = new File(
                         pathComboBox.getSelectionModel().getSelectedItem(),
                         filename.getText());
@@ -63,17 +118,11 @@ public class CreateFileDialog extends Dialog<Pair<String, String>> {
                 return new Pair<>(pathComboBox.getSelectionModel().getSelectedItem(), filename.getText());
             }
             return null;
-        });
+        };
+    }
 
-        super.getDialogPane().setContent(grid);
-
-        Node createButton = super.getDialogPane().lookupButton(create);
-        filename.textProperty().addListener((observable, oldValue, newValue) ->
-                createButton.setDisable(newValue.trim().isEmpty()));
-        createButton.setDisable(true);
-        filename.requestFocus();
-        Optional<Pair<String, String>> result = showAndWait();
-        result.ifPresent(o -> {
+    protected Consumer<Pair<String, String>> confirmHandler() {
+        return o -> {
             File target = new File(o.getKey(), o.getValue());
             template.build(target.getAbsolutePath());
             if (pathComboBox.getSelectionModel().getSelectedIndex() == 0) {
@@ -82,6 +131,6 @@ public class CreateFileDialog extends Dialog<Pair<String, String>> {
                 right.reload();
             }
             super.close();
-        });
+        };
     }
 }
--- gui/src/main/java/pl/greywarden/openr/gui/dialogs/NewFileDialog.java
+++ gui/src/main/java/pl/greywarden/openr/gui/dialogs/NewFileDialog.java
@@ -1,52 +1,33 @@
 package pl.greywarden.openr.gui.dialogs;
 
-import javafx.geometry.Insets;
-import javafx.scene.Node;
-import javafx.scene.control.ButtonBar;
-import javafx.scene.control.ButtonType;
 import javafx.scene.control.ComboBox;
-import javafx.scene.control.Dialog;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.ColumnConstraints;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Priority;
 import javafx.util.Pair;
-import javafx.util.StringConverter;
 import pl.greywarden.openr.gui.directoryview.DirectoryView;
-import pl.greywarden.openr.i18n.I18nManager;
 import pl.greywarden.openr.templates.Template;
 
 import java.io.File;
-import java.util.Optional;
+import java.util.function.Consumer;
 
-public class NewFileDialog extends Dialog<Pair<String, String>> {
+public class NewFileDialog extends CreateFileDialog {
+
+    private ComboBox<Template> templates;
 
     public NewFileDialog(DirectoryView left, DirectoryView right) {
-        I18nManager i18n = I18nManager.getInstance();
-        i18n.setBundle("new-file-dialog");
-        super.setTitle(i18n.getString("title"));
-        super.setHeaderText(i18n.getString("header"));
+        super(null, left, right);
+        createHeaderAndTitle();
+    }
 
-        ButtonType create = new ButtonType(i18n.getString("create"), ButtonBar.ButtonData.OK_DONE);
-        ButtonType cancel = new ButtonType(i18n.getString("cancel"), ButtonBar.ButtonData.CANCEL_CLOSE);
-        super.getDialogPane().getButtonTypes().addAll(create, cancel);
+    @Override
+    protected void createDialogContent() {
+        super.createDialogContent();
+        createTemplatesComboBox();
+        getGrid().addRow(2, new Label(i18n.getString("file-type") + ":"), templates);
+    }
 
-        GridPane grid = new GridPane();
-        grid.setHgap(10);
-        grid.setVgap(10);
-        grid.setPadding(new Insets(20, 10, 10, 10));
-
-        TextField filename = new TextField();
-        filename.setPromptText(i18n.getString("filename"));
-
-        ComboBox<String> pathComboBox = new ComboBox<>();
-        pathComboBox.getItems().addAll(left.getRootPath(), right.getRootPath());
-        pathComboBox.getSelectionModel().select(0);
-        pathComboBox.setPrefWidth(400);
-
-        ComboBox<Template> templates = new ComboBox<>();
+    private void createTemplatesComboBox() {
+        templates = new ComboBox<>();
         templates.getItems().addAll(Template.getAvailableTemplates());
         templates.setButtonCell(new ListCell<Template>() {
             @Override
@@ -70,41 +51,13 @@ public class NewFileDialog extends Dialog<Pair<String, String>> {
                 }
             }
         });
-
-        grid.addRow(0, new Label(i18n.getString("filename") + ":"), filename);
-        grid.addRow(1, new Label(i18n.getString("path") + ":"), pathComboBox);
-        grid.addRow(2, new Label(i18n.getString("file-type") + ":"), templates);
-
-        ColumnConstraints stretch = new ColumnConstraints();
-        stretch.setFillWidth(true);
-        grid.getColumnConstraints().addAll(stretch);
-        GridPane.setFillWidth(templates, true);
-
-
-        super.getDialogPane().setContent(grid);
-
         templates.getSelectionModel().select(0);
-        templates.setPrefWidth(400);
+        templates.setMinWidth(400);
+    }
 
-        super.setResultConverter(dialogButton -> {
-            if (dialogButton == create) {
-                File target = new File(
-                        pathComboBox.getSelectionModel().getSelectedItem(),
-                        filename.getText());
-                if (target.exists()) {
-                    return null;
-                }
-                return new Pair<>(pathComboBox.getSelectionModel().getSelectedItem(), filename.getText());
-            }
-            return null;
-        });
-
-        Node createButton = super.getDialogPane().lookupButton(create);
-        filename.textProperty().addListener((observable, oldValue, newValue) ->
-                createButton.setDisable(newValue.trim().isEmpty()));
-        createButton.setDisable(true);
-        filename.requestFocus();
-        showAndWait().ifPresent(o -> {
+    @Override
+    protected Consumer<Pair<String, String>> confirmHandler() {
+        return o -> {
             File target = new File(o.getKey(), o.getValue());
             templates.getSelectionModel().getSelectedItem().build(target.getAbsolutePath());
             if (pathComboBox.getSelectionModel().getSelectedIndex() == 0) {
@@ -113,7 +66,13 @@ public class NewFileDialog extends Dialog<Pair<String, String>> {
                 right.reload();
             }
             super.close();
-        });
+        };
     }
 
+    @Override
+    protected void createHeaderAndTitle() {
+        i18n.setBundle("new-file-dialog");
+        super.setTitle(i18n.getString("title"));
+        super.setHeaderText(i18n.getString("header"));
+    }
 }

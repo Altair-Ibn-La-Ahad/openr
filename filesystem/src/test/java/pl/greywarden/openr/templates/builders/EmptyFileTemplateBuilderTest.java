833ba5e9ca1a212fa86a2f755743d3f25dbc2ad6
Marcin Las <las.marcin.94@gmail.com>
2017-02-07 09:15:41 +0100

    Created tests for template builders

---
+++ filesystem/src/test/java/pl/greywarden/openr/templates/builders/EmptyFileTemplateBuilderTest.java
@@ -0,0 +1,42 @@
+package pl.greywarden.openr.templates.builders;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.SystemUtils;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+import java.nio.file.Files;
+
+public class EmptyFileTemplateBuilderTest {
+
+    private String testDirectory;
+
+    @Before
+    public void setUp() throws Exception {
+        testDirectory = SystemUtils.USER_HOME + File.separator + "test-dir";
+        Files.createDirectory(new File(testDirectory).toPath());
+    }
+
+    @Test
+    public void build() throws Exception {
+        // given
+        AbstractTemplateFileBuilder builder = new EmptyFileTemplateBuilder(getTestFileName());
+        // when
+        builder.build();
+        // then
+        Assert.assertTrue(new File(getTestFileName() + "").exists());
+    }
+
+    private String getTestFileName() {
+        return testDirectory + File.separator + "test";
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        FileUtils.deleteDirectory(new File(testDirectory));
+    }
+
+}

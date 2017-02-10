a4458ce6739c468a745bc543253c291e76c1ed67
Marcin Las <las.marcin.94@gmail.com>
2017-02-10 16:03:48 +0100

    Favourite program remove dialog

--- gui/src/main/java/pl/greywarden/openr/gui/scenes/main_window/menu/favourite_programs/RemoveDialog.java
+++ gui/src/main/java/pl/greywarden/openr/gui/scenes/main_window/menu/favourite_programs/RemoveDialog.java
@@ -1,16 +1,10 @@
 package pl.greywarden.openr.gui.scenes.main_window.menu.favourite_programs;
 
-import javafx.geometry.Pos;
-import javafx.scene.control.Button;
 import javafx.scene.control.Dialog;
-import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.Priority;
+import javafx.scene.input.KeyCode;
 import javafx.stage.StageStyle;
-import pl.greywarden.openr.commons.IconManager;
 import pl.greywarden.openr.gui.dialogs.CommonButtons;
 import pl.greywarden.openr.gui.favourite_programs.FavouritePrograms;
 import pl.greywarden.openr.gui.favourite_programs.Program;
@@ -28,21 +22,18 @@ public class RemoveDialog extends Dialog<Boolean> {
             protected void updateItem(Program item, boolean empty) {
                 super.updateItem(item, empty);
                 if (!empty) {
-                    HBox wrapper = new HBox();
-                    Pane spacer = new Pane();
-                    Label label = new Label(item.getName());
-                    Button removeButton = new Button();
-                    removeButton.setGraphic(IconManager.getIcon("delete-permanent-small"));
-                    removeButton.setOnAction(event -> {
-                        FavouritePrograms.remove(item);
-                        parent.invalidate();
-                        programs.getItems().remove(item);
-                    });
-                    HBox.setHgrow(spacer, Priority.ALWAYS);
-                    wrapper.setAlignment(Pos.CENTER);
-                    wrapper.getChildren().setAll(label, spacer, removeButton);
-                    setGraphic(wrapper);
+                    setText(item.getName());
+                } else {
+                    setText("");
                 }
+            }
+        });
+        programs.setOnKeyPressed(event -> {
+            if (KeyCode.DELETE.equals(event.getCode())) {
+                Program selectedItem = programs.getSelectionModel().getSelectedItem();
+                programs.getItems().remove(selectedItem);
+                FavouritePrograms.remove(selectedItem);
+                parent.invalidate();
             }
         });
 

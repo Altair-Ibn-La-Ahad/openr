746d78ff23c39f73cc45b3468971ab206b929841
Marcin Las <las.marcin.94@gmail.com>
2017-02-16 17:30:04 +0100

    Resizeable image preview

--- gui/src/main/java/pl/greywarden/openr/gui/dialogs/PreviewImageDialog.java
+++ gui/src/main/java/pl/greywarden/openr/gui/dialogs/PreviewImageDialog.java
@@ -3,8 +3,10 @@ package pl.greywarden.openr.gui.dialogs;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.ScrollEvent;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -16,17 +18,38 @@ import static pl.greywarden.openr.commons.I18nManager.getString;
 
 public class PreviewImageDialog extends Stage {
 
+    private ImageView imageView;
+    private StackPane imagePane;
+
     public PreviewImageDialog(File imageFile) {
-        super();
+        super(StageStyle.UTILITY);
         super.setTitle(getString("preview"));
-        super.initStyle(StageStyle.UTILITY);
-        ImageView imageView = getImageViewFromFile(imageFile);
-        StackPane imagePane = createImagePane(imageView);
-        scaleToFitWindow(imageView);
-        imagePane.getChildren().setAll(imageView);
-        super.setScene(new Scene(imagePane));
+        createPreviewImageDialogLayout(imageFile);
+        ScrollPane wrapper = new ScrollPane(imagePane);
+        wrapper.addEventFilter(ScrollEvent.SCROLL, this::scaleImagePreview);
+        super.setScene(new Scene(wrapper));
         centerOnScreen();
         show();
+    }
+
+    private void createPreviewImageDialogLayout(File imageFile) {
+        imageView = getImageViewFromFile(imageFile);
+        imagePane = createImagePane(imageView);
+        imagePane.getChildren().setAll(imageView);
+    }
+
+    private void scaleImagePreview(ScrollEvent event) {
+        double delta = event.getDeltaY();
+        if (canScale(delta)) {
+            imageView.setFitWidth(imageView.getFitWidth() + delta);
+            imageView.setFitHeight(imageView.getFitHeight() + delta);
+            imageView.setPreserveRatio(true);
+        }
+        event.consume();
+    }
+
+    private boolean canScale(double delta) {
+        return delta > 0 || imageView.getFitWidth() >= super.getWidth();
     }
 
     private StackPane createImagePane(ImageView imageView) {
@@ -41,6 +64,7 @@ public class PreviewImageDialog extends Stage {
         ImageView imageView;
         try {
             imageView = new ImageView(imageFile.toURI().toURL().toString());
+            scaleToFitWindow(imageView);
         } catch (MalformedURLException e) {
             throw new RuntimeException(e);
         }
@@ -49,12 +73,17 @@ public class PreviewImageDialog extends Stage {
 
     private void scaleToFitWindow(ImageView imageView) {
         Image image = imageView.getImage();
-        double scaleFactor = image.widthProperty().greaterThan(500).get()
-                ? image.getWidth() / 500.0
-                : 1.0;
+        double scaleFactor = getScaleFactor(image);
         double imageWidth = imageView.getImage().getWidth();
         double imageHeight = imageView.getImage().getHeight();
         imageView.fitWidthProperty().setValue(imageWidth / scaleFactor);
         imageView.fitHeightProperty().setValue(imageHeight / scaleFactor);
+    }
+
+    private double getScaleFactor(Image image) {
+        int MAX_IMAGE_PREVIEW_WIDTH = 500;
+        return image.widthProperty().greaterThan(MAX_IMAGE_PREVIEW_WIDTH).get()
+                ? (image.getWidth() / MAX_IMAGE_PREVIEW_WIDTH)
+                : 1.0;
     }
 }

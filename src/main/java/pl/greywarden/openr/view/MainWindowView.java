b918ef7b6bbb57b2531141a4bf7370d2ef2f553c
Marcin Las <las.marcin.94@gmail.com>
2020-05-09 18:28:32 +0200

    Added exit feature

--- src/main/java/pl/greywarden/openr/view/MainWindowView.java
+++ src/main/java/pl/greywarden/openr/view/MainWindowView.java
@@ -2,6 +2,8 @@ package pl.greywarden.openr.view;
 
 import de.saxsys.mvvmfx.FxmlView;
 import de.saxsys.mvvmfx.InjectViewModel;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
@@ -11,4 +13,7 @@ public class MainWindowView implements FxmlView<MainWindowViewModel> {
     @InjectViewModel
     private MainWindowViewModel mainWindowViewModel;
 
+    public void exitApplication() {
+        Platform.runLater(Platform::exit);
+    }
 }

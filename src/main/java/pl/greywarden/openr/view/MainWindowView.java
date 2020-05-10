9cd91896b2d5df0cf2423d0ec45af48e5dbbee08
Marcin Las <las.marcin.94@gmail.com>
2020-05-10 08:55:31 +0200

    [OR-16] Added "About" dialog

--- src/main/java/pl/greywarden/openr/view/MainWindowView.java
+++ src/main/java/pl/greywarden/openr/view/MainWindowView.java
@@ -5,6 +5,7 @@ import de.saxsys.mvvmfx.InjectViewModel;
 import javafx.application.Platform;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.Label;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
@@ -24,5 +25,16 @@ public class MainWindowView implements FxmlView<MainWindowViewModel> {
                 .showAndWait()
                 .filter(ButtonType.YES::equals)
                 .ifPresent(b -> Platform.runLater(Platform::exit));
+    }
+
+    public void showAboutDialog() {
+        var appVersion = mainWindowViewModel.getApplicationVersion();
+        var content = new Label(String.format("OpenR file manager, version: %s%n%nAuthor: Marcin Las%nEmail: las.marcin.94@gmail.com", appVersion));
+        var aboutDialog = new Alert(Alert.AlertType.INFORMATION);
+        aboutDialog.setTitle("About");
+        aboutDialog.setHeaderText("OpenR");
+        aboutDialog.getDialogPane().setContent(content);
+        
+        aboutDialog.show();
     }
 }
--- src/main/java/pl/greywarden/openr/view/MainWindowViewModel.java
+++ src/main/java/pl/greywarden/openr/view/MainWindowViewModel.java
@@ -1,8 +1,17 @@
 package pl.greywarden.openr.view;
 
 import de.saxsys.mvvmfx.ViewModel;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 @Component
+@PropertySource("classpath:/app-info.properties")
 public class MainWindowViewModel implements ViewModel {
+    @Value("${app-version}")
+    private String applicationVersion;
+
+    String getApplicationVersion() {
+        return applicationVersion;
+    }
 }

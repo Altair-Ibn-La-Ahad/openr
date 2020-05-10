5ce970ac1f95e67315150a11fbc86ce291058a95
Marcin Las <las.marcin.94@gmail.com>
2020-05-10 11:09:28 +0200

    [OR-5] Added selected file text field to status bar

--- src/main/java/pl/greywarden/openr/view/MainWindowView.java
+++ src/main/java/pl/greywarden/openr/view/MainWindowView.java
@@ -3,19 +3,34 @@ package pl.greywarden.openr.view;
 import de.saxsys.mvvmfx.FxmlView;
 import de.saxsys.mvvmfx.InjectViewModel;
 import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 @Slf4j
 @Component
-public class MainWindowView implements FxmlView<MainWindowViewModel> {
+public class MainWindowView implements FxmlView<MainWindowViewModel>, Initializable {
+    @FXML
+    private TextField selectedFile;
+
     @InjectViewModel
     private MainWindowViewModel mainWindowViewModel;
 
-    public void exitApplication() {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        selectedFile.textProperty().bindBidirectional(mainWindowViewModel.selectedFileProperty());
+    }
+
+    @FXML
+    private void exitApplication() {
         var confirmationDialog = new Alert(Alert.AlertType.CONFIRMATION);
         confirmationDialog.getButtonTypes().setAll(ButtonType.YES, ButtonType.NO);
         confirmationDialog.setTitle("Confirm exit");
@@ -27,7 +42,8 @@ public class MainWindowView implements FxmlView<MainWindowViewModel> {
                 .ifPresent(b -> Platform.runLater(Platform::exit));
     }
 
-    public void showAboutDialog() {
+    @FXML
+    private void showAboutDialog() {
         var appVersion = mainWindowViewModel.getApplicationVersion();
         var content = new Label(String.format("OpenR file manager, version: %s%n%nAuthor: Marcin Las%nEmail: las.marcin.94@gmail.com", appVersion));
         var aboutDialog = new Alert(Alert.AlertType.INFORMATION);
--- src/main/java/pl/greywarden/openr/view/MainWindowViewModel.java
+++ src/main/java/pl/greywarden/openr/view/MainWindowViewModel.java
@@ -1,17 +1,34 @@
 package pl.greywarden.openr.view;
 
 import de.saxsys.mvvmfx.ViewModel;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 @Component
 @PropertySource("classpath:/app-info.properties")
-public class MainWindowViewModel implements ViewModel {
+class MainWindowViewModel implements ViewModel {
+    private final StringProperty selectedFileProperty = new SimpleStringProperty();
+
     @Value("${app-version}")
     private String applicationVersion;
 
     String getApplicationVersion() {
         return applicationVersion;
     }
+
+    StringProperty selectedFileProperty() {
+        return selectedFileProperty;
+    }
+
+    String getSelectedFile() {
+        return selectedFileProperty.getValue();
+    }
+
+    void setSelectedFile(String selectedFile) {
+        this.selectedFileProperty.setValue(selectedFile);
+    }
+
 }
